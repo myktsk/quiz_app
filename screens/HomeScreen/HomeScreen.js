b2b846625b22ceeecee496ecae4d4ac5731d7217
myktsk <22167483+myktsk@users.noreply.github.com>
2025-03-11 20:53:31 -0400

    :recycle: make icons reusable

--- screens/HomeScreen/HomeScreen.js
+++ screens/HomeScreen/HomeScreen.js
@@ -9,34 +9,7 @@ import {
 } from "react-native";
 import { globalStyles } from "../../shared/GlobalStyles";
 import { COLORS, QUIZ_CATEGORIES, ROUTES } from "../../shared/constants";
-
-const categoryIcons = {
-  "question-mark": require("../../assets/category-icons/question-mark.png"),
-  globe: require("../../assets/category-icons/globe.png"),
-  "open-book": require("../../assets/category-icons/open-book.png"),
-  "book-stack": require("../../assets/category-icons/book-stack.png"),
-  movie: require("../../assets/category-icons/movie.png"),
-  "vinyl-record": require("../../assets/category-icons/vinyl-record.png"),
-  theater: require("../../assets/category-icons/theater.png"),
-  television: require("../../assets/category-icons/television.png"),
-  joystick: require("../../assets/category-icons/joystick.png"),
-  "board-game": require("../../assets/category-icons/board-game.png"),
-  chemistry: require("../../assets/category-icons/chemistry.png"),
-  computer: require("../../assets/category-icons/computer.png"),
-  calculator: require("../../assets/category-icons/calculator.png"),
-  "greek-mythology": require("../../assets/category-icons/greek-mythology.png"),
-  sports: require("../../assets/category-icons/sports.png"),
-  castle: require("../../assets/category-icons/castle.png"),
-  politician: require("../../assets/category-icons/politician.png"),
-  palette: require("../../assets/category-icons/palette.png"),
-  "guest-star": require("../../assets/category-icons/guest-star.png"),
-  hen: require("../../assets/category-icons/hen.png"),
-  truck: require("../../assets/category-icons/truck.png"),
-  "comic-book": require("../../assets/category-icons/comic-book.png"),
-  "smart-watch": require("../../assets/category-icons/smart-watch.png"),
-  ghost: require("../../assets/category-icons/ghost.png"),
-  cartoon: require("../../assets/category-icons/cartoon.png"),
-};
+import { categoryIcons } from "../../shared/icons";
 
 export const HomeScreen = ({ navigation }) => {
   const [selectedCategory, setSelectedCategory] = useState(null);
