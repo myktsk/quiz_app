8f0c57d55ca515980581d33fee9255031d1ffddb
makoto082 <makototsuga@gmail.com>
2025-04-07 10:12:17 -0400

    ✨ Implemented api functionality

--- screens/QuizScreen/QuizScreen.js
+++ screens/QuizScreen/QuizScreen.js
@@ -4,46 +4,123 @@ import {
   StyleSheet,
   FlatList,
   TouchableOpacity,
+  ActivityIndicator, // スピナー
 } from 'react-native';
 import { COLORS } from '../../shared/constants';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import * as Progress from 'react-native-progress';
-
-const quize = {
-  type: 'multiple',
-  difficulty: 'easy',
-  category: 'Sports',
-  question: 'Which two teams played in Super Bowl XLII?',
-  correct_answer: 'The New York Giants & The New England Patriots',
-  incorrect_answers: [
-    'The Green Bay Packers & The Pittsburgh Steelers',
-    'The Philadelphia Eagles & The New England Patriots',
-    'The Seattle Seahawks & The Denver Broncos',
-  ],
-};
+import { useRoute } from '@react-navigation/native';
+import { decode } from 'html-entities';
+import { Alert } from 'react-native';
+import { ROUTES } from '../../shared/constants';
 
 export const QuizScreen = ({ navigation }) => {
   const [selectedAnswer, setSelectedAnswer] = useState(null);
-  const [progress, setProgress] = useState(0);
+  const [questions, setQuestions] = useState([]);
+  const [count, setCount] = useState(0);
+  const [correctCount, setCorrectCount] = useState(0);
+  const [choices, setChoices] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const route = useRoute();
+  const { category } = route.params;
+
+  useEffect(() => {
+    const fetchQuizData = async () => {
+      setLoading(true);
+      try {
+        let url = `https://opentdb.com/api.php?amount=10&type=multiple`;
+
+        if (category !== 'any') {
+          url += `&category=${category}`;
+        }
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if (data.results && Array.isArray(data.results)) {
+          setQuestions(data.results);
+
+          // Shuffle choices
+          const shuffledQuestions = data.results.map((quize) => {
+            const options = [
+              quize.correct_answer,
+              ...quize.incorrect_answers,
+            ].map(decode);
+            const shuffledOptions = options.sort(() => Math.random() - 0.5);
+
+            return shuffledOptions;
+          });
+
+          setChoices(shuffledQuestions);
+        } else {
+          fetchQuizData(); // Re-fetch Because it may not be able to acquire the data.
+        }
+      } catch (error) {
+        console.error('API fetch error:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchQuizData();
+  }, []);
 
   const handlePress = (item) => {
     setSelectedAnswer(item);
   };
 
-  const handleNext = () => {
-    if (progress < 1) {
-      setProgress(progress + 0.1);
+  const handleAnswer = () => {
+    if (selectedAnswer === null) {
+      Alert.alert('Please select an answer！');
+      return;
     }
+
+    const correctAnswer = questions[count]?.correct_answer;
+    if (selectedAnswer === correctAnswer) {
+      setCorrectCount(correctCount + 1);
+      Alert.alert('Correct！', 'Congratulations！', [
+        { text: 'Next question', onPress: () => handleNext() },
+      ]);
+    } else {
+      Alert.alert('Incorrect！', `Correct answer is ${decode(correctAnswer)}`, [
+        { text: 'Next question', onPress: () => handleNext() },
+      ]);
+    }
+    if (count < 9) {
+      setCount(count + 1);
+    }
+    setSelectedAnswer(null);
   };
+
+  const handleNext = () => {
+    if (count < 9) {
+      return;
+    }
+    navigation.navigate(ROUTES.SCORE_BOARD, {
+      category: category,
+      correctCount: correctCount,
+    });
+  };
+
+  if (loading) {
+    return (
+      <View style={styles.loadingWrapper}>
+        <ActivityIndicator size='large' color={COLORS.PRIMARY} />
+        <Text style={styles.loadingText}>Loading...</Text>
+      </View>
+    );
+  }
 
   return (
     <View style={styles.screenWrapper}>
       <View style={styles.questionArea}>
-        <Text style={styles.questionAreaText}>Q2. {quize.question}</Text>
+        <Text style={styles.questionAreaText}>
+          Q{count + 1}. {decode(questions[count]?.question)}
+        </Text>
       </View>
 
       <FlatList
-        data={[quize.correct_answer, ...quize.incorrect_answers]}
+        data={choices[count]}
         renderItem={({ item }) => (
           <TouchableOpacity onPress={() => handlePress(item)}>
             <View
@@ -76,19 +153,16 @@ export const QuizScreen = ({ navigation }) => {
           width: '100%',
         }}
       >
-        <TouchableOpacity
-          style={styles.nextButton}
-          onPress={handleNext} // Nextボタンを押すと進捗が更新される
-        >
-          <Text style={styles.nextButtonText}>→Next</Text>
+        <TouchableOpacity style={styles.nextButton} onPress={handleAnswer}>
+          <Text style={styles.nextButtonText}>Answer</Text>
         </TouchableOpacity>
       </View>
       <View style={{ width: '100%' }}>
         <Progress.Bar
-          width={null} // Set width to null to allow it to expand to container's width
+          width={null}
           height={10}
-          progress={progress} // Reflect progress
-          color={progress === 1 ? 'red' : 'blue'}
+          progress={count * 0.1}
+          color={count * 0.1 === 1 ? 'red' : 'blue'}
           unfilledColor='lightgray'
           borderRadius={5}
           animationType='timing'
@@ -155,5 +229,15 @@ const styles = new StyleSheet.create({
   nextButtonText: {
     color: COLORS.WHITE,
     fontWeight: 'bold',
+  },
+  loadingWrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loadingText: {
+    marginTop: 16,
+    fontSize: 18,
+    color: COLORS.PRIMARY,
   },
 });
